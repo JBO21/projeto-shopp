e3ff19713358692eb1b92e3b1c894132efc04c00
JBO21 <jackson.jbo@gmail.com>
2023-03-23 18:28:46 -0300

    ajustando o header

--- shoppbarth/urls.py
+++ shoppbarth/urls.py
@@ -1,9 +1,7 @@
 from django.urls import path
 
-from shoppbarth.views import contato, home, sobre
+from shoppbarth.views import home
 
 urlpatterns = [
-    path('', home),
-    path('sobre/', sobre),
-    path('contato/', contato),
+    path('', home),   
 ]
--- shoppbarth/views.py
+++ shoppbarth/views.py
@@ -3,10 +3,4 @@ from django.shortcuts import render
 
 
 def home(request):
-    return HttpResponse("Home Casa Sei la")
-
-def contato(request):
-    return HttpResponse("CONTATO")
-
-def sobre(request):
-    return HttpResponse("SOBRE")
+    return render(request, 'shoppbarth/pages/home.html')
